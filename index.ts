7a7a0892c2c3f0906868810c65c397271a8f7137
rumman <139452817+Rummantouqeer@users.noreply.github.com>
2023-10-10 07:19:02 -0700

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from 'chalk';
+import chalkAnimation, { rainbow } from 'chalk-animation';
+import showBanner from 'node-banner';
+import choices from 'inquirer/lib/objects/choices.js';
+
+
+    
+
+console.log(chalk.bgBlue("Guess a number between 1 to 10")); 
+let actualNumber:number=9;
+let NumTries:number = 3
+
+
+while (NumTries>0) {
+    const answers = await inquirer.prompt([
+
+    {
+        name:"YourGuess",
+        type:"number",
+        message:"Enter a number",
+    },
+])
+
+
+    console.log(answers);
+    if(answers.YourGuess==actualNumber ){
+        console.log(chalk.bgBlue("You guessed the right number"));
+        NumTries==0;
+    }
+    else
+    {
+        console.log(chalk.bgBlue("You guessed the wrong number"));
+    }
+    if (actualNumber>answers.YourGuess) {
+        console.log(chalk.bgBlue("Think Higher"));
+    }
+     else{
+        console.log(chalk.bgBlue("Think Lower"));
+        }
+        console.log(chalk.bgBlue(`You have ${NumTries-1} left`));
+
+NumTries--;
+    }
+
+
+
+
+
+    
+    
